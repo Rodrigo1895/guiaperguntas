eafc096273c21fa280665112fc38656ac66e741c
Rodrigo Lima <rodrigo_rossetti_lima@hotmail.com>
2020-03-02 21:46:14 -0300

    Aula 33

--- index.js
+++ index.js
@@ -4,28 +4,12 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
-    var produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca-Cola", preco: 5},
-        {nome: "Leite", preco: 1.45},
-        {nome: "Carne", preco: 15},
-        {nome: "Redbull", preco: 6},
-        {nome: "Nescau", preco: 4}
-    ]
-
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do Programador",
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
 });
 
 app.listen(8080, ()=> {
