6860b632313ef002d7e11ba87c34cbd345d0d142
Rodrigo Lima <rodrigo_rossetti_lima@hotmail.com>
2020-03-03 23:27:33 -0300

    Aula 50

--- index.js
+++ index.js
@@ -1,15 +1,70 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
+
 const app = express();
 
+// Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o banco de dados!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    });
+
+//Configurando  Express usar EJS como view engine
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Rotas
 app.get("/", (req, res) => {
-    res.render("index");
+    Pergunta.findAll({ 
+        raw: true, 
+        order: [
+            ['id', 'DESC']
+        ]}).then(perguntas => {
+        res.render("index", {
+            perguntas: perguntas
+        });
+    });
 });
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/");
+    });
+});
+
+app.get("/pergunta/:id", (req, res) => {
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta => {
+        if(pergunta != undefined) {
+            res.render("pergunta", {
+                pergunta: pergunta
+            });
+        } else {
+            res.redirect("/");
+        }
+    });
 });
 
 app.listen(8080, ()=> {
